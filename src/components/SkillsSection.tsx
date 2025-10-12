47b368c735a875fc0772ebb61154b0eda7956ab0
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-10-12 07:45:25 +0700

    add animation in SkillsSection component

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -1,16 +1,112 @@
 import TechStack from "./TechStack";
 import techStack from "../data/techStack";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { Observer } from "gsap/Observer";
+import { SplitText } from "gsap/SplitText";
+import { TechStackContext } from "../contexts/techStack";
 
 const SkillsSection = () => {
+	const sectionRef = useRef<null | HTMLElement>(null);
+	const headingRef = useRef<null | HTMLHeadingElement>(null);
+	const techStackListRef = useRef<null | HTMLUListElement>(null);
+
+	useEffect(() => {
+		gsap.registerPlugin(Observer);
+		const sectionEl: null | HTMLElement = sectionRef.current;
+		const headingEl: null | HTMLHeadingElement = headingRef.current;
+		const techStackListEl: null | HTMLUListElement = techStackListRef.current;
+
+		if (headingEl && techStackListEl && sectionEl) {
+			const splitHeading: SplitText = new SplitText(headingEl, {
+				type: "chars, words, lines",
+				linesClassName: "clip-text",
+			});
+
+			let hasRun: boolean = false;
+
+			interface GoToSection {
+				(): void;
+			}
+
+			const goToSection: GoToSection = () => {
+				const timeline = gsap.timeline({
+					defaults: { during: 1.25, ease: "power1.inOut" },
+					onStart: () => {
+						hasRun = true;
+					},
+				});
+
+				timeline
+					.fromTo(
+						sectionEl,
+						{
+							autoAlpha: 0,
+							yPercent: 150,
+						},
+						{
+							autoAlpha: 1,
+							yPercent: 0,
+							duration: 0.6,
+							ease: "power2",
+						},
+					)
+					.fromTo(
+						splitHeading.chars,
+						{
+							yPercent: 30,
+							autoAlpha: 0.6,
+						},
+						{
+							yPercent: 0,
+							autoAlpha: 1,
+							duration: 0.8,
+							ease: "power2",
+						},
+						0.3,
+					)
+					.fromTo(
+						techStackListEl,
+						{
+							autoAlpha: 0.6,
+							yPercent: 30,
+						},
+						{
+							autoAlpha: 1,
+							yPercent: 0,
+							duration: 0.8,
+							ease: "power2",
+						},
+						0.3,
+					);
+			};
+
+			Observer.create({
+				type: "wheel, touch, pointer",
+				wheelSpeed: -0.5,
+				onUp: () => !hasRun && goToSection(),
+				tolerance: 10,
+			});
+		}
+	}, []);
+
 	return (
 		<section
+			ref={sectionRef}
 			className="bg-bg-gr-1 flex min-h-[100dvh] w-full items-center 
 		p-[64px]">
 			<div
 				className="bg-gr-icy flex flex-col items-start gap-[32px] 
 			rounded-[16px] p-[32px]">
-				<h2 className="text-h2 text-bg-1 font-bold">Tech-Stack</h2>
-				<TechStack techStack={techStack} />
+				<h2 ref={headingRef} className="text-h2 text-bg-1 font-bold">
+					Tech-Stack
+				</h2>
+				<TechStackContext
+					value={{
+						techStackListRef: techStackListRef,
+					}}>
+					<TechStack techStack={techStack} />
+				</TechStackContext>
 			</div>
 		</section>
 	);
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,16 +1,21 @@
-import type { FC } from "react";
+import { useContext, type FC } from "react";
 import type { Tech } from "../types/types";
+import { TechStackContext } from "../contexts/techStack";
 
 interface TechStackProps {
 	techStack: Tech[];
 }
 
 const TechStack: FC<TechStackProps> = ({ techStack }) => {
+	const { techStackListRef } = useContext(TechStackContext);
+
 	return (
-		<ul className="flex flex-wrap gap-[32px]">
+		<ul ref={techStackListRef} className="flex flex-wrap gap-[32px]">
 			{techStack.map((tech: Tech) => {
 				return (
-					<li className="bg-bg-1 flex items-center gap-[16px] rounded-[8px] px-[32px] py-[16px]">
+					<li
+						className={`bg-bg-1 flex items-center gap-[16px] rounded-[8px] 
+							px-[32px] py-[16px]`}>
 						<img width="48px" height="48px" src={tech.icon} />
 						{tech.text}
 					</li>
