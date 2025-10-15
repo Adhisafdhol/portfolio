a262c025bbd58d564d33da66675671a2f661f544
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-10-15 21:02:12 +0700

    aadd Project types

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,4 +3,20 @@ interface Tech {
 	text: string;
 }
 
-export type { Tech };
+interface ProjectImage {
+	url: string;
+	description: string;
+	alt: string;
+	type: string;
+}
+
+interface Project {
+	title: {
+		content: string;
+		styles: string;
+	};
+	about: string;
+	images: ProjectImage[];
+}
+
+export type { Tech, ProjectImage, Project };
