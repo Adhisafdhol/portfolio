a7016ec8649aec83f7f323c3ca9e0ae2419842ed
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-10-18 05:34:59 +0700

    integrate ProjectSection into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import HeroHeader from "./components/HeroHeader";
 import SkillsSection from "./components/SkillsSection";
+import ProjectSection from "./components/ProjectsSection";
 
 function App() {
 	return (
 		<>
 			<HeroHeader />
 			<SkillsSection />
+			<ProjectSection />
 		</>
 	);
 }
