c267508510761d17ce56b1538fdd31f5262fdac5
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-10-17 23:28:04 +0700

    add Video component

---
+++ src/components/Video.tsx
@@ -0,0 +1,117 @@
+import { useRef, type FC, useState, useEffect } from "react";
+
+type VideoRef = React.RefObject<null | HTMLVideoElement>;
+
+interface Callback {
+	//eslint-disable-next-line
+	(_val: boolean): void;
+}
+
+const toggleVideo = (videoRef: VideoRef, callback: Callback): void => {
+	const videoEl: null | HTMLVideoElement = videoRef.current;
+
+	if (videoEl) {
+		if (videoEl.paused || videoEl.ended) {
+			videoEl.play();
+			callback(true);
+		} else {
+			videoEl.pause();
+			callback(false);
+		}
+	}
+};
+
+interface VideoProps {
+	src: string;
+	type: string;
+}
+
+const Video: FC<VideoProps> = ({ src, type }) => {
+	const videoRef = useRef<null | HTMLVideoElement>(null);
+	const [isPlaying, setIsPlaying] = useState<boolean>(false);
+	const [isControlShown, setIsControlShown] = useState<boolean>(true);
+	const timeoutIds = useRef<number[]>([]);
+
+	useEffect(() => {
+		const updatePlayState = (): void => {
+			setIsPlaying(false);
+		};
+
+		const videoEl: null | HTMLVideoElement = videoRef.current;
+
+		if (videoEl) {
+			videoEl.addEventListener("ended", updatePlayState);
+		}
+
+		return () => {
+			if (videoEl) {
+				videoEl.removeEventListener("ended", updatePlayState);
+			}
+		};
+	}, []);
+
+	const clearTimeouts = (): void => {
+		timeoutIds.current.forEach((id: number) => {
+			clearTimeout(id);
+		});
+		timeoutIds.current = [];
+	};
+
+	const hideVideoController = (): void => {
+		setIsControlShown(false);
+	};
+
+	const showVideoController = (): void => {
+		clearTimeouts();
+		setIsControlShown(true);
+		const id = setTimeout(hideVideoController, 3000);
+		timeoutIds.current = [...timeoutIds.current, id];
+	};
+
+	const handleMouseLeave = (): void => {
+		clearTimeouts();
+		if (isPlaying) {
+			setIsControlShown(false);
+		} else {
+			setIsControlShown(true);
+		}
+	};
+
+	return (
+		<div
+			className="relative flex items-center justify-center rounded-[16px]"
+			onMouseEnter={() => {
+				showVideoController();
+			}}
+			onMouseMove={() => {
+				showVideoController();
+			}}>
+			<video
+				ref={videoRef}
+				className="rounded-[16px]"
+				onMouseLeave={handleMouseLeave}>
+				<source src={src} type={type} />
+			</video>
+			<button
+				className={
+					`icon play-icon absolute flex h-[128px] w-[128px] items-center
+          justify-center rounded-full bg-[rgba(7,7,7,0.6)] backdrop-blur-2xl
+          hover:bg-[rgba(234,243,250,0.3)]` +
+					(isPlaying ? " pause-icon" : " play-icon") +
+					(isControlShown ? "" : " hidden")
+				}
+				aria-label={isPlaying ? "Pause video" : "Play video"}
+				onClick={() =>
+					toggleVideo(videoRef, (val: boolean) => {
+						setIsPlaying(val);
+						clearTimeouts();
+						const id = setTimeout(hideVideoController, 3000);
+						timeoutIds.current = [...timeoutIds.current, id];
+					})
+				}
+			/>
+		</div>
+	);
+};
+
+export default Video;
