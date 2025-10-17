5635ba09019c8f2f25026c1620664f3bfdda0013
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-10-18 05:34:45 +0700

    add ProjectsSection component

---
+++ src/components/ProjectsSection.tsx
@@ -0,0 +1,13 @@
+import projects from "../data/projects";
+import Projects from "./Projects";
+
+const ProjectSection = () => {
+	return (
+		<section className="flex flex-col gap-[64px] p-[64px]">
+			<h2 className="text-h2 text-start font-black">Projects</h2>
+			<Projects projects={projects} />
+		</section>
+	);
+};
+
+export default ProjectSection;
