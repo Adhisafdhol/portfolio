a8d668fcd0c6c954d74dec97fe3ad42181698a15
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-10-18 00:01:49 +0700

    add Project component

---
+++ src/components/Project.tsx
@@ -0,0 +1,30 @@
+import type { FC } from "react";
+import type { Project as ProjectI } from "../types/types";
+import ProjectPreviewBox from "./ProjectPreviewBox";
+
+interface ProjectProps {
+	project: ProjectI;
+}
+
+const Project: FC<ProjectProps> = ({ project }) => {
+	return (
+		<article className="flex flex-col items-start gap-[32px]">
+			<h3
+				className={
+					"text-h4 bg-linear-to-r bg-clip-text font-extrabold text-[transparent]" +
+					` ${project.title.styles}`
+				}>
+				{project.title.content}
+			</h3>
+			<p className="text-h6 text-fg-2 max-w-[72ch] text-start font-bold">
+				{project.about}
+			</p>
+			<ProjectPreviewBox
+				previews={project.previews}
+				styles={project.title.styles}
+			/>
+		</article>
+	);
+};
+
+export default Project;
