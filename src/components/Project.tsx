0e4f314af2d266feb8cb78edde1b0fa8d11ca999
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-10-24 11:44:27 +0700

    render project's links on Project component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,7 +1,8 @@
 import type { FC } from "react";
-import type { Project as ProjectI } from "../types/types";
+import type { Project as ProjectI, ProjectLink } from "../types/types";
 import ProjectPreviewBox from "./ProjectPreviewBox";
 import ProjectTechStack from "./ProjectTechStack";
+import ProjectLinkButton from "./ProjectLinkButton";
 
 interface ProjectProps {
 	project: ProjectI;
@@ -9,7 +10,7 @@ interface ProjectProps {
 
 const Project: FC<ProjectProps> = ({ project }) => {
 	return (
-		<article className="flex flex-col items-start gap-[32px]">
+		<article className="flex flex-col items-start gap-[32px] py-[32px]">
 			<h3
 				className={
 					`text-h4 bg-linear-to-r bg-clip-text font-extrabold
@@ -20,8 +21,16 @@ const Project: FC<ProjectProps> = ({ project }) => {
 			<p className="text-h6 text-fg-2 max-w-[72ch] text-start font-bold">
 				{project.about}
 			</p>
+
 			<ProjectPreviewBox previews={project.previews} />
-			<ProjectTechStack techStack={project.techStack} />
+			<div>
+				{project.links.map((link: ProjectLink) => {
+					return <ProjectLinkButton link={link} />;
+				})}
+			</div>
+			{project.techStack.length ? (
+				<ProjectTechStack techStack={project.techStack} />
+			) : null}
 		</article>
 	);
 };
