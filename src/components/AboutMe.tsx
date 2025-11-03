13bfb0b49023cf3b8a5182de597520186ed6c15d
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-11-03 20:24:55 +0700

    add AboutMe component

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,5 +1,70 @@
+import { useRef } from "react";
+import aboutMe from "../data/aboutMe";
+
 const AboutMe = () => {
-	return <></>;
+	const buttonIconRef = useRef<null | HTMLButtonElement>(null);
+	return (
+		<section className="bg-gr-bg-1-b flex flex-col items-start gap-[32px] p-[32px]">
+			<h2
+				className={`text-h4 bg-gr-brand-main bg-clip-text font-bold 
+				text-[transparent]`}>
+				About me
+			</h2>
+			<div>
+				{aboutMe.profile.map((paragraph: string): React.JSX.Element => {
+					return (
+						<>
+							<p className="text-h6 text-fg-2 max-w-[72ch] text-start">
+								{paragraph}
+							</p>
+							<br />
+						</>
+					);
+				})}
+			</div>
+			<button
+				onMouseEnter={(e: React.MouseEvent<HTMLButtonElement>) => {
+					const button: HTMLButtonElement = e.target as HTMLButtonElement;
+					button.classList.remove("animate-button-out");
+					button.classList.add("animate-button-in");
+					buttonIconRef.current?.classList.remove("animate-button-icon-out");
+					buttonIconRef.current?.classList.add(
+						"animate-button-icon-in",
+						"scale-150",
+						"translate-x-[8px]",
+					);
+				}}
+				onMouseLeave={(e: React.MouseEvent<HTMLButtonElement>) => {
+					const button: HTMLButtonElement = e.target as HTMLButtonElement;
+					button.classList.remove("animate-button-in");
+					button.classList.add("animate-button-out");
+					buttonIconRef.current?.classList.remove(
+						"animate-button-icon-in",
+						"scale-150",
+						"translate-x-[8px]",
+					);
+					buttonIconRef.current?.classList.add("animate-button-icon-out");
+				}}
+				className={`bg-gr-project-btn text-h5 text-fg-1  
+							hover:animate-button-in h-h6 flex gap-[8px] 
+							rounded-[4px] bg-linear-to-r bg-size-[300%] px-[48px] 
+							py-[24px] hover:rounded-[16px] hover:bg-position-[100%]`}>
+				<div className="flex items-center gap-[16px]">
+					<a href={aboutMe.github.url}>{aboutMe.github.text}</a>
+					<img
+						src={aboutMe.github.icon}
+						alt={`${aboutMe.github.text} icon`}
+						className="h-[1.5em] min-h-[1.5em] w-[1.5em] min-w-[1.5em]"
+					/>
+				</div>
+
+				<span
+					ref={buttonIconRef}
+					className={`icon visit-arrow-icon animate-button-icon flex h-[1.5em]
+							w-[1.5em] items-center`}></span>
+			</button>
+		</section>
+	);
 };
 
 export default AboutMe;
