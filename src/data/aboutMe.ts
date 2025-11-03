ed61819ba8e5071dceab99d271fb4c5340309bf7
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-11-03 20:22:36 +0700

    add AboutMe data

---
+++ src/data/aboutMe.ts
@@ -0,0 +1,43 @@
+import githubIcon from "../assets/images/github-icon.svg";
+import type { AboutMe } from "../types/types";
+
+const aboutMe: AboutMe = {
+	profile: [
+		`I am a self-taught developer who loves developing products that
+  solve real-world problems, starting from their initial ideation, research,
+  and design process, to the development. I love the art of creating things,
+  from drawing, painting, and other things that involve craftsmanship, which is
+  what drove me into learning about programming in early 2022. The way software
+  development requires collaborative efforts and combined knowledge from vast
+  fields in order to deliver a product to the user drew me into this field,
+  where I can put my love of the art of creation into developing something
+  practical.`,
+		`My current process in software development is to take an idea and then
+  research the "hows" and "whys" the product exists and find the best way to
+  bring the vision to life. While I mainly focus on the programming side of the
+  equation, I love all the things that comprise the software development
+  process, from branding, UI/UX, and project management. I love learning new
+  things in my journey as a developer. In fact, I learnt about branding and
+  UI/UX to bring my personality to life through this portfolio site, because I
+  believe a good user experience requires not just performance but also how the
+  product looks and what kind of story it evokes.`,
+		`On the technical side, my philosophy is to build a system that is
+  maintainable, performant, and well-tested. I think it’s crucial for me as a
+  developer to not just cater for the user side of the equation but also to
+  create a good developer experience, by writing clean code that is easy to
+  understand and developing a testing strategy to ease future refactors and
+  feature updates.`,
+		`There are so many things I still need to learn in this journey of becoming a
+  better developer. While the learning never stops, I am intrigued by the many
+  possibilities that this field can bring to help people solve and ease their
+  daily activities. Currently, I am delving more into real-time communication
+  systems and security.`,
+	],
+	github: {
+		text: "Check my work on Github",
+		icon: githubIcon,
+		url: "https://github.com/Adhisafdhol",
+	},
+};
+
+export default aboutMe;
