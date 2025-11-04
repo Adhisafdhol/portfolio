20ec0a464d39fdcde403f4e3a97194cd71f01ce1
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-11-04 18:24:19 +0700

    add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+	return (
+		<footer className="p-[16px]">
+			<p className="text-s">
+				© Copyright Adhis Afdhol 2025. All rights reserved
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
