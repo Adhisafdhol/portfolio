3255033c9e9799380a08fd6b4a0cb8a686591ce5
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-11-05 20:33:58 +0700

    make InputContainer responsive

--- src/components/InputContainer.tsx
+++ src/components/InputContainer.tsx
@@ -6,7 +6,9 @@ interface InputContainer {
 
 const InputContainer: FC<InputContainer> = ({ children }) => {
 	return (
-		<div className="flex w-[72ch] max-w-[72ch] flex-col items-start gap-[32px]">
+		<div
+			className="flex w-[min(72ch,100%)] max-w-[72ch] flex-col items-start
+		gap-[32px]">
 			{children}
 		</div>
 	);
