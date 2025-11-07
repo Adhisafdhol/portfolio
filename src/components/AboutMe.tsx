5b6554f439ddbf5b5b1b4d921710acad0e333115
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-11-07 21:25:27 +0700

    make AboutMe component mobile responsive

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,7 +4,9 @@ import aboutMe from "../data/aboutMe";
 const AboutMe = () => {
 	const buttonIconRef = useRef<null | HTMLButtonElement>(null);
 	return (
-		<section className="bg-gr-bg-1-b flex flex-col items-start gap-[32px] p-[32px]">
+		<section
+			className="bg-gr-bg-1-b flex flex-col items-start gap-[32px] 
+		p-[16px] sm:p-[64px]">
 			<h2
 				className={`text-h4 bg-gr-brand-main bg-clip-text font-bold 
 				text-[transparent]`}>
@@ -15,7 +17,9 @@ const AboutMe = () => {
 					(paragraph: string, index: number): React.JSX.Element => {
 						return (
 							<Fragment key={index}>
-								<p className="text-h6 text-fg-2 max-w-[72ch] text-start">
+								<p
+									className="text-p sm:text-h6 text-fg-2 max-w-[72ch]
+								text-start">
 									{paragraph}
 								</p>
 								<br />
@@ -47,10 +51,11 @@ const AboutMe = () => {
 					);
 					buttonIconRef.current?.classList.add("animate-button-icon-out");
 				}}
-				className={`bg-gr-project-btn text-h5 text-fg-1  
+				className={`bg-gr-project-btn text-h6 sm:text-h5 text-fg-1  
 							hover:animate-button-in h-h6 flex gap-[8px] 
-							rounded-[4px] bg-linear-to-r bg-size-[300%] px-[48px] 
-							py-[24px] hover:rounded-[16px] hover:bg-position-[100%]`}>
+							rounded-[4px] bg-linear-to-r bg-size-[300%] px-[32px] py-[16px] 
+							hover:rounded-[16px] hover:bg-position-[100%] sm:px-[48px]
+							sm:py-[24px]`}>
 				<div className="flex items-center gap-[16px]">
 					<a href={aboutMe.github.url}>{aboutMe.github.text}</a>
 					<img
