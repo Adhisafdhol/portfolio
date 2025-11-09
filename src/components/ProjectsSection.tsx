76dd32c53d77be806e725dbdc28959eedad06f0e
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-11-09 19:00:51 +0700

    improve ProjectsSection UI on mobile

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -3,7 +3,9 @@ import Projects from "./Projects";
 
 const ProjectSection = () => {
 	return (
-		<section className="flex flex-col gap-[64px] p-[16px] sm:p-[64px]">
+		<section
+			className="flex flex-col gap-[32px] p-[16px] sm:gap-[64px]
+				sm:p-[64px]">
 			<h2 className="text-h2 text-start font-black">Projects</h2>
 			<Projects projects={projects} />
 		</section>
