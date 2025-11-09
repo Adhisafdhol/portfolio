1424c159f26054e201bd0568958cc8917abf2a70
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-11-09 18:59:19 +0700

    improve TechStack UI on mobile

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -12,7 +12,7 @@ const TechStack: FC<TechStackProps> = ({ techStack }) => {
 	return (
 		<ul
 			ref={techStackListRef}
-			className="flex flex-wrap gap-[16px] sm:gap-[32px]">
+			className="flex flex-wrap gap-[16px] md:gap-[24px] lg:gap-[32px]">
 			{techStack.map((tech: Tech, index: number) => {
 				return (
 					<li
@@ -20,7 +20,7 @@ const TechStack: FC<TechStackProps> = ({ techStack }) => {
 						className={`bg-bg-1 flex items-center gap-[16px] rounded-[8px] 
 							px-[24px] py-[16px] sm:px-[32px] sm:py-[16px]`}>
 						<img
-							className="h-[24px] w-[24px] sm:h-[32px] sm:w-[32ppx]  md:h-[40px]
+							className="h-[16px] w-[16px] sm:h-[32px] sm:w-[32ppx] md:h-[40px]
 							md:w-[40px] lg:h-[48px] lg:w-[48px]"
 							src={tech.icon.dark}
 						/>
