f1e7c0e2cdc9d0c70b7ab08fd029bf2c80d48f6b
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-11-09 19:00:11 +0700

    improve ContactMe UI on mobile

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -4,7 +4,7 @@ const ContactMe = () => {
 	return (
 		<section
 			className={`flex min-h-[min(100dvh,1080px)] flex-col items-start
-      justify-center gap-[32px] p-[16px] sm:p-[64px]`}>
+      justify-center gap-[32px] px-[16px] py-[64px] sm:p-[64px]`}>
 			<h2 className="text-h4">Contact Me</h2>
 			<ContactForm />
 		</section>
