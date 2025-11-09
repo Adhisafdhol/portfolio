dfa34ff70f89b7863b0383e7bf7e93712ab1f3ff
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-11-09 18:55:47 +0700

    improve InputContainer UI on mobile

--- src/components/InputContainer.tsx
+++ src/components/InputContainer.tsx
@@ -8,7 +8,7 @@ const InputContainer: FC<InputContainer> = ({ children }) => {
 	return (
 		<div
 			className="flex w-[min(72ch,100%)] max-w-[72ch] flex-col items-start
-		gap-[32px]">
+		gap-[16px] sm:gap-[32px]">
 			{children}
 		</div>
 	);
